26e6c4824ac239e735ddd86962e9bf906b960df3
muhsin66 <165603222+muhsin66@users.noreply.github.com>
2024-03-31 22:15:56 +0100

    Added files to repository

---
+++ classes.js
@@ -0,0 +1,89 @@
+let products = [
+    {
+      id: 1,
+       "title": "Musin",
+       "price": 20.00,
+       "images": "images/cooking.jpg",
+       spaces: 10,
+      
+    },
+    {
+      id: 2,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       "image": "images/cooking.jpg",
+       "spaces": 10,
+      
+    },
+    {
+      id: 3,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/jquery.jpg",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 4,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/sports.jpg",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 5,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/robotics.jpg",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 6,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/html.jpg",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 7,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/driving",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 8,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/dance",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 9,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       image: "images/creative",
+       spaces: 10,
+      
+    },
+
+    {
+      id: 10,
+       title: "Cat Food, 25lb bag",
+       price: 20.00,
+       "image": "images/math",
+       "spaces": 10,
+      
+    },
+  ]
